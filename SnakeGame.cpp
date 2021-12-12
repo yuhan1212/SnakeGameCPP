03fc3f7e6b06f6382f31e1ac9ed0b429488ee631
hannah <yuhan.chi12@gmail.com>
2021-12-12 11:53:03 -0800

    [SnakeGameCPP] add Setup and Draw

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -20,7 +20,7 @@ void Setup() {
     noecho();
     cbreak();
     curs_set(0);
-    
+
 	gameOver = false;
 	dir = STOP;
 	x = width / 2;
@@ -28,6 +28,48 @@ void Setup() {
 	fruitX = rand() % width;
 	fruitY = rand() % height;
 	score = 0;
+}
+
+void Draw() {
+	system("clear");
+	for (int i = 0; i < width + 2; i++) 
+		cout << "#";
+	cout << endl;
+
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j ++) {
+			if (j == 0)
+				cout << "#";
+
+			if (i == y && j == x)
+				cout << "o";
+
+			else if (i == fruitY && j == fruitX)
+				cout << "F";
+
+			else {
+				bool print = false;
+				for (int k = 0; k < nTail; k ++) {
+					if (tailX[k] == j && tailY[k] == i) {
+						cout << "o";
+						print = true;
+					}
+				}
+				if (!print)
+					cout << " ";
+			}
+
+			if (j == width - 1) 
+				cout << "#";
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < width + 2; i++) 
+		cout << "#";
+	cout << endl;
+	cout << "Score: " << score << endl;
+
 }
 
 

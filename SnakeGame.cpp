320d2f895f65531d62fd22dea7bd03ff06a93c3c
hannah <yuhan.chi12@gmail.com>
2021-12-12 11:54:18 -0800

    [SnakeGameCPP] add Logic

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -96,6 +96,52 @@ void Input() {
 	}
 }
 
+void Logic() {
+	int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+    switch (dir) {
+	    case LEFT:
+	        x--;
+	        break;
+	    case RIGHT:
+	        x++;
+	        break;
+	    case UP:
+	        y--;
+	        break;
+	    case DOWN:
+	        y++;
+	        break;
+	    default:
+	        break;
+    }
+    if (x >= width) x = 0; else if (x < 0) x = width - 1;
+    if (y >= height) y = 0; else if (y < 0) y = height - 1;
+ 
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+ 
+    if (x == fruitX && y == fruitY)
+    {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
+}
+
 
 
 int main() {
